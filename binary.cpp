483fdc704edb3d0b84ae082e2ff28dc0bd2bb91c
Param Khatod ( Jain ) <121992570+paramkhatod@users.noreply.github.com>
2025-02-13 13:51:15 +0530

    Update binary.cpp

--- binary.cpp
+++ binary.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
-    cout<<"Enter the size of array =>";
+    cout<<"Enter array size: ";
     cin>>n;
-    
     int arr[n];
-    cout<<"Enter "<<n<<" Integers in ascending order => \n";
-    for(int i=0;i<n;i++)
-        cin>>arr[i];    
-    
-    int target;
-    cout<<"Enter the number you want to search => ";
-    cin>>target;
+    for(int i=0;i<n;i++){
+        cout<<"Enter No: ";
+        cin>>arr[i];
+    }
+    int key;
+    cout<<"Enter No. Wnts to find: ";
+    cin>>key;
     
     int start = 0, end = n-1, mid;
     int pos= -1;
@@ -23,20 +21,22 @@ int main()
     {
         mid = (start+end)/2;
         
-        if(arr[mid]==target){
+        if(arr[mid]==key){
             pos = mid;
             break;
         }
-        else if(arr[mid]>target)
+        else if(arr[mid]>key)
             end = mid -1;   
         else
             start = mid + 1;
     }
     
-    if(pos==-1)
-        cout<<target<<" is not present in the array"<<endl;
-    else
-        cout<<target<<" is present in the array at position "<<pos<<endl;
-        
+    
+    if(pos == -1){
+        cout<<key<<" is not Present in array"<<endl;
+    }else{
+        cout<<key<<" is present at "<<pos<<endl;
+    }
+
     return 0;
 }
